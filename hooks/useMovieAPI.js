fe0d661de06e43b3cd4943a1f8e02c6224fff5c8
AlexandraHutchby <alexandrahutchby@gmail.com>
2025-06-04 08:59:56 +1200

    testing for movie api

---
+++ hooks/useMovieAPI.js
@@ -0,0 +1,47 @@
+export const validateApiConfig = () => {
+
+};
+
+export const validateApiResponse = () => {
+
+};
+
+export const makeRequest = () => {
+
+};
+
+export const discoverContent = () => {
+
+};
+
+export const getPopularContent = () => {
+
+};
+
+export const getTrendingContent = () => {
+
+};
+
+export const searchContent = () => {
+
+};
+
+export const getContentDetails = () => {
+
+};
+
+export const formatContentForList = () => {
+
+};
+
+export const generateWatchList = () => {
+
+};
+
+export const StreamingProviders = () => {
+
+};
+
+export const ProviderNames = () => {
+
+};
